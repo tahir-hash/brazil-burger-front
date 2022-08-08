a9c90023beb5c182de18151169fbde16fd1de8c4
tahir-hash <mbayepro6@gmail.com>
2022-08-08 03:51:02 +0000

    quartier serv

--- src/app/client/commande/pages/panier/panier.component.ts
+++ src/app/client/commande/pages/panier/panier.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Cart } from 'src/app/shared/models/cart';
 import { Produit } from 'src/app/shared/models/produit';
+import { Quartier } from 'src/app/shared/models/quartier';
 import { CartService } from 'src/app/shared/services/cart.service';
+import { QuartiersService } from 'src/app/shared/services/quartiers.service';
 
 @Component({
   selector: 'mtm-panier',
@@ -13,8 +15,8 @@ export class PanierComponent implements OnInit {
   items:Cart={
     all:[]
   };
- 
-  constructor(private cart:CartService) { }
+ qrt:Quartier[]=[]
+  constructor(private cart:CartService,private quartier:QuartiersService) { }
   
   ngOnInit(): void {
     this.cart.Panier.subscribe(data=>{
@@ -23,10 +25,25 @@ export class PanierComponent implements OnInit {
       }
       this.items=data
     })
-  
+    
+    this.quartier.getQuartier$().subscribe(quart=>{
+      this.qrt=quart
+    })
   }
   removeToCart(obj:any) {
     this.cart.remove(obj);
+  }
+
+
+  //nav tabs
+  activeTab: string = 'search';
+
+  search(activeTab: string) {
+    this.activeTab = activeTab;
+  }
+
+  result(activeTab: string) {
+    this.activeTab = activeTab;
   }
 
 }
---
+++ src/app/shared/models/zone.ts
@@ -0,0 +1,6 @@
+
+export interface Zone{
+    id:number;
+    libelle:string;
+    prix: number;
+}
